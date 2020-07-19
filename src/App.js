48e2456f08f287502ef705f116ac3b91a1e04f68
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-07-20 00:03:44 +0300

    refactor: move main logic to new list component

--- src/App.js
+++ src/App.js
@@ -1,56 +1,16 @@
-import React, { useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
-import Item from "./Item";
+import React from "react";
+import useItemState from "./hooks/useItemState";
 import NewItemInput from "./NewItemInput";
-import { readStorage, updateStorage } from "./storageHelpers";
-import List from "@material-ui/core/List";
-
-import "./App.css";
+import ItemList from "./ItemList";
 
 function App() {
-  const [items, itemsSet] = useState(readStorage("items") || []);
-
-  useEffect(() => updateStorage("items", items));
-
-  const handlers = {
-    add: (text) => {
-      if (!text) return;
-
-      const newItem = {
-        id: uuidv4(),
-        text,
-        isChecked: false,
-      };
-
-      itemsSet([...items, newItem]);
-    },
-
-    check: (id) => {
-      itemsSet(
-        items.map((el) =>
-          el.id === id ? { ...el, isChecked: !el.isChecked } : el
-        )
-      );
-    },
-
-    edit: (id, text) => {
-      itemsSet(items.map((el) => (el.id === id ? { ...el, text } : el)));
-    },
-
-    remove: (id) => {
-      itemsSet(items.filter((el) => el.id !== id));
-    },
-  };
-
-  const list = items.map((item) => (
-    <Item key={item.id} {...item} {...handlers} />
-  ));
+  const { items, handlers } = useItemState();
 
   return (
-    <div className='App'>
+    <>
       <NewItemInput handleAdd={handlers.add} />
-      <List dense>{list}</List>
-    </div>
+      <ItemList items={items} handlers={handlers} />
+    </>
   );
 }
 
