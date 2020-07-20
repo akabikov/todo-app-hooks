b8c1810a5a68911a833efe7103df2c71822d24b8
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-07-20 19:19:41 +0300

    feat: add a button to close editing

--- src/ItemEdit.jsx
+++ src/ItemEdit.jsx
@@ -5,6 +5,7 @@ import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import IconButton from "@material-ui/core/IconButton";
 import TextField from "@material-ui/core/TextField";
 import SaveIcon from "@material-ui/icons/Save";
+import CloseIcon from "@material-ui/icons/Close";
 
 function ItemEdit({ id, text, edit, toggleEditMode }) {
   const [editedText, handleChange] = useInputState(text);
@@ -22,6 +23,9 @@ function ItemEdit({ id, text, edit, toggleEditMode }) {
         <ListItemSecondaryAction>
           <IconButton aria-label='save' type='submit'>
             <SaveIcon />
+          </IconButton>
+          <IconButton aria-label='close' onClick={toggleEditMode}>
+            <CloseIcon />
           </IconButton>
         </ListItemSecondaryAction>
       </form>
