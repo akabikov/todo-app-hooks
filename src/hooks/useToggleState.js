6b7af9567d0395f7285055d9381973b6323a66e6
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-07-22 00:41:07 +0300

    refactor: change to functional state update

--- src/hooks/useToggleState.js
+++ src/hooks/useToggleState.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 function useToggleState(initVal = false) {
   const [state, setState] = useState(initVal);
-  const toggle = () => setState(!state);
+  const toggle = () => setState((st) => !st);
 
   return [state, toggle];
 }
