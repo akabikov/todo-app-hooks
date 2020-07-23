1f48f7324da571c370df109151597b7d1fb07295
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-07-23 20:14:06 +0300

    Updates

---
+++ precache-manifest.dd0fbe1777c7264c5466a60bcf9e6866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569936d2460e4d5b29dfcff57249b76a",
+    "url": "/todo-app-hooks/index.html"
+  },
+  {
+    "revision": "af67969c7244ec34d107",
+    "url": "/todo-app-hooks/static/css/main.d731fec1.chunk.css"
+  },
+  {
+    "revision": "1a147172f8dd90b7c770",
+    "url": "/todo-app-hooks/static/js/2.a8ec2d0e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-app-hooks/static/js/2.a8ec2d0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af67969c7244ec34d107",
+    "url": "/todo-app-hooks/static/js/main.7dc82962.chunk.js"
+  },
+  {
+    "revision": "329d6ed2dd212ca0d8ed",
+    "url": "/todo-app-hooks/static/js/runtime-main.e0a03659.js"
+  }
+]);
