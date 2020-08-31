da9a1974e0a78aa1fda83f7715d4b917389f002f
Alexey Kabikov <kabikov.a.s@gmail.com>
2020-08-31 23:37:11 +0300

    fix: update localstorage data

--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
+import { updateStorage } from "../helpers/storageHelpers";
 import Paper from "@material-ui/core/Paper";
 import Item from "./Item";
 import List from "@material-ui/core/List";
 
 function ItemList({ items }) {
+  useEffect(() => updateStorage("items", items), [items]);
+
   if (!items.length) return null;
 
   const list = items.map((item, i) => (
